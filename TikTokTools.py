9dd670d68d1bacfb1c5f580d1b5c34e0f0ae62cd
Andrew Hossack <andrew_hossack@outlook.com>
2021-03-02 22:06:07 -0800

    Uniqueness checker

--- TikTokTools.py
+++ TikTokTools.py
@@ -18,6 +18,7 @@ class TikTokTools():
         self.requested_length = 0
         # Verbosity 0 = no print
         # Verbosity 1 = print statements
+        # Verbosity 2 = extra verbose
         self._verbosity = verbosity
 
     def _check_video_shorter_than(self, video_entry):
@@ -26,19 +27,26 @@ class TikTokTools():
         '''
         res = video_entry['video']['duration'] < self.requested_length
         if self._verbosity:
-            print("Video shorter than {self.requested_length}: {res}")
+            print(f"Video shorter than {self.requested_length}: {res}")
         return res
 
     def _check_video_not_in_list(self, video):
         '''
         Returns boolean if video being checked is in self.videos
         '''
+        not_in_list = None
         for entry in self.videos:
-            if video == entry:
-                if self._verbosity:
-                    print("Video in list, retrieving new one...")
-                return False
-        return True
+            if self._verbosity == 2:
+                print(f'_check_video_not_in_list video:\n\n{video}\n\nentry:\n\n{entry}')
+            if video['id'] == entry['id']:
+                not_in_list = False
+            else:
+                not_in_list =  True
+        if not self.videos:
+            not_in_list = True
+        if self._verbosity:
+            print(f'not_in_list {not_in_list}')
+        return not_in_list
 
     def get_video_list(self, num_videos_requested, length_seconds):
         '''
@@ -50,12 +58,24 @@ class TikTokTools():
             self._add_video()
         return self.videos
 
-    def _add_video(self, ):
+    def _add_video(self):
+        '''
+        Add new video to self.videos list
+        '''
+
+        # TODO BUG every time a new video is requested, it always retrieves
+        # the first video in a list somewhere that is always the same. Need
         video = self.api.trending(count=1, custom_verifyFp="")[0]
-        if self._check_video_shorter_than(video) and self._check_video_not_in_list(video): 
+        # to retrieve unique video.
+
+        not_in_list = self._check_video_not_in_list(video)
+        is_shorter = self._check_video_shorter_than(video)
+        added_video = None
+        if (is_shorter and not_in_list): 
             self.videos.append(video)
-            if self._verbosity:
-                print("Added new video")
-            return True
+            added_video = True
         else:
-            return False
+            added_video = False
+        if self._verbosity:
+            print(f'added_video {added_video}')
+        return added_video
