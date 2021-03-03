eee542d2feaef4f499acced646ba9f072a058769
Andrew Hossack <andrew_hossack@outlook.com>
2021-03-02 21:44:34 -0800

    Video checking methods, gitignore

--- TikTokTools.py
+++ TikTokTools.py
@@ -6,27 +6,56 @@
 
 from TikTokApi import TikTokApi
 
-class TikTok():
+class TikTokTools():
     '''
     TikTok Tools Class leverages TikTokApi heavily
 
     https://github.com/davidteather/TikTok-Api
     '''
-    def __init__(self, **kwargs):
+    def __init__(self, verbosity=0, **kwargs):
         self.api = TikTokApi(**kwargs)
+        self.videos = []
+        self.requested_length = 0
+        # Verbosity 0 = no print
+        # Verbosity 1 = print statements
+        self._verbosity = verbosity
 
+    def _check_video_shorter_than(self, video_entry):
+        '''
+        Returns boolean if video is less than given length
+        '''
+        res = video_entry['video']['duration'] < self.requested_length
+        if self._verbosity:
+            print("Video shorter than {self.requested_length}: {res}")
+        return res
 
-if __name__ == "__main__":
-    instance = TikTok()
+    def _check_video_not_in_list(self, video):
+        '''
+        Returns boolean if video being checked is in self.videos
+        '''
+        for entry in self.videos:
+            if video == entry:
+                if self._verbosity:
+                    print("Video in list, retrieving new one...")
+                return False
+        return True
 
-    results = 10
+    def get_video_list(self, num_videos_requested, length_seconds):
+        '''
+        Return video list
+        videos (list): list of dictionary tiktok objects
+        '''
+        self.requested_length = length_seconds
+        while len(self.videos) < num_videos_requested:
+            self._add_video()
+        return self.videos
 
-    # Since TikTok changed their API you need to use the custom_verifyFp option. 
-    # In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
-    trending = instance.api.trending(count=results, custom_verifyFp="")
-
-    for tiktok in trending:
-        # Prints the id of the tiktok
-        print(tiktok['id'])
-
-    print(len(trending))
+    def _add_video(self, ):
+        video = self.api.trending(count=1, custom_verifyFp="")[0]
+        if self._check_video_shorter_than(video) and self._check_video_not_in_list(video): 
+            self.videos.append(video)
+            if self._verbosity:
+                print("Added new video")
+            return True
+        else:
+            return False
---
+++ main.py
@@ -0,0 +1,19 @@
+from TikTokTools import TikTokTools
+import dat.exampledict
+
+
+
+
+def main(**kwargs):
+    api = TikTokTools(verbosity=1)
+    num_videos_requested = kwargs.get('num_videos_requested', 5)
+    length_seconds = kwargs.get('length_seconds', 20)
+
+    videos = api.get_video_list(num_videos_requested, length_seconds)
+
+    for tiktok in videos:
+        # Prints the id of the tiktok
+        print(f"Title: {tiktok['desc']} by {tiktok['author']['nickname']}\nLink: {tiktok['video']['playAddr']}\n\n")
+
+if __name__ == "__main__":
+    main()
